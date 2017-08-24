0f42cb5e661c1e60252b8f26dd3ec2f2e39e5b92
Niko van Meurs <nikovanmeurs@gmail.com>
2017-08-24 21:50:50 +0200

    feat: Add team section

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import './index.html';
 import './css/index.css';
 import './img/hero.jpg';
 import './img/found-ico.png';
+
+import './img/team-akaramanlis.jpg';
+import './img/team-imos.jpg';
+import './img/team-jxu.jpg';
+import './img/team-kbasten.jpg';
+import './img/team-nvmeurs.jpg';
+
+import * as moment from 'moment';
+
