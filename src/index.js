cd8247a4fd3b1be4e006281ea4b0c134f3cc1c95
rene <rene@anonymous.nl>
2017-09-04 19:45:20 +0200

    Improve Korean site and add Hindi language

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './index.html';
 import './ko.html';
+import './hi.html';
 import './css/index.css';
 import './img/hero.jpg';
 import './img/found-ico.png';
@@ -40,25 +41,66 @@ import * as ScrollReveal from 'scrollreveal';
 
 require('smoothscroll-polyfill').polyfill();
 
-const targetDate = moment('2017-09-01T18:00:00Z');
+const targetDate = moment('2017-09-21T18:00:00Z');
 const countdownHolder = document.createElement('div');
 let countdownInterval;
 
 const translations = {
   'en': {
-    'days':    'days',
-    'hours':   'hours',
-    'minutes': 'minutes',
-    'seconds': 'seconds',
-    'until':   'until ICO',
+    'started':    'The ICO has started',
+    'contribute': 'Contribute now',
+    'live':       'The contribution period is now live and it will end on the 28th of September 2017 at approximately 18:00 GMT, or when the hard cap has been reached. To contribute you must agree to our terms and click on “Contribute”.',
+    'days':       'days',
+    'hours':      'hours',
+    'minutes':    'minutes',
+    'seconds':    'seconds',
+    'until':      'until ICO',
+    'buying':     'Buying tokens',
+    'karmorate':  'Karmo tokens can be bought at a fixed rate of <span class="u-bold">1 ETH = 30.000 KRM</span>.',
+    'tobuy':      'To buy tokens, send the necessary Ether ot the Opakeco wallet using the information on the side.',
+    'howto':      'Are you ready to buy Karmo tokens but unsure of how to do so?',
+    'walkthrough': 'View walkthrough',
+    'walletaddr': 'Wallet address',
+    'gasprice':   'Gas price',
+    'gaslimit':   'Recommended gas limit',
   },
 
   'ko': {
-    'days':    '일',
-    'hours':   '시간',
-    'minutes': '분',
-    'seconds': '초',
-    'until':   'ICO 까지',
+    'started':    'ICO가 시작되었습니다',
+    'contribute': '투자 참여',
+    'live':       '투자가 현재 진행중이며 2017년 9월 28일 GMT 기준 약 18:00에 종료되거나 하드캡 목표치에 이르게 되면 투자가 종료됩니다.',
+    'days':       '일',
+    'hours':      '시간',
+    'minutes':    '분',
+    'seconds':    '초',
+    'until':      'ICO 까지',
+    'buying':     '토큰 구매',
+    'karmorate':  '카르모 토큰은 <span class="u-bold">1이더리움당30,000 KRM의</span> 고정 비율로 구매 가능합니다.',
+    'tobuy':      '토큰을 구매하려면 필요한 이더를 측면에 안내되어 있는 오파케코 지갑으로 보내세요.',
+    'howto':      '카르모 토큰을 구매할 준비가 되었지만 어떻게 할지 모르겠나요?',
+    'walkthrough': '참여 방법 안내',
+    'walletaddr': '지갑 주소',
+    'gasprice':   '가스 가격',
+    'gaslimit':   '추천 가스 한도',
+  },
+
+  'hi': {
+    'started':    'आईसीओ शुरू कर दिया है',
+    'contribute': 'अब योगदान करें',
+    'live':       'योगदान की अवधि अब जी रही है और यह 28 सितम्बर 2017 को लगभग 18:00 जीएमटी, या जब हार्ड कैप तक पहुंच गया है, समाप्त हो जाएगा।.',
+    'days':       'दिन',
+    'hours':      'घंटे',
+    'minutes':    'मिनट',
+    'seconds':    'सेकंड',
+    'until':      'आईसीओ तक',
+    'buying':     'टोकन ख़रीदना',
+    'karmorate':  'कार्मो टोकन 1 ई<span class="u-bold">1 ईथ = 30.000 केआरएम</span> की एक निश्चित दर से खरीदा जा सकता है.',
+    'tobuy':      'टोकन खरीदने के लिए, पक्ष के बारे में जानकारी का उपयोग करके ओपकेको वॉलेट के लिए आवश्यक ईथर ओटी भेजें।.',
+    'howto':      'क्या आप कर्मो टोकन खरीदने के लिए तैयार हैं लेकिन ऐसा करने के बारे में अनिश्चित हैं?',
+    'walkthrough': 'देखें वॉकथ्रू',
+    'walletaddr': 'बटुआ पता',
+    'gasprice':   'गैस की कीमत',
+    'gaslimit':   'अनुशंसित गैस सीमा',
   }
 };
 
@@ -119,27 +161,27 @@ function handleCheckboxChange(event) {
 function handleFormSubmit(event) {
   event.preventDefault();
   document.querySelector('.Section--contribute .Section-content').innerHTML = `
-    <h2 class="Heading Section-heading">Buying tokens</h2>
+    <h2 class="Heading Section-heading">${translated('buying')}</h2>
     <div class="Contribute-sectionWrapper">
       <div class="Contribute-section">
-        <p class="Text">Karmo tokens can be bought at a fixed rate of <span class="u-bold">1 ETH = 30.000 KRM</span>.</p>
-        <p class="Text">To buy tokens, send the necessary Ether ot the Opakeco wallet using the information on the side.</p>
-        <p class="Text">Are you ready to buy Karmo tokens but unsure of how to do so?</p>
+        <p class="Text">${translated('karmorate')}</p>
+        <p class="Text">${translated('tobuy')}</p>
+        <p class="Text">${translated('howto')}</p>
         <div class="Section-actions">
-          <a class="Button Section-action" href="/media/ico-walkthrough.pdf" target="_blank">View walkthrough</a>
+          <a class="Button Section-action" href="/media/ico-walkthrough.pdf" target="_blank">${translated('walkthrough')}</a>
         </div>
       </div>
       <div class="Contribute-section">
         <div class="Contribute-field">
-          <div class="Contribute-title">Wallet address</div>
+          <div class="Contribute-title">${translated('walletaddr')}</div>
           <div class="Contribute-value">tbd</div>
         </div>
         <div class="Contribute-field">
-          <div class="Contribute-title">Gas price</div>
+          <div class="Contribute-title">${translated('gasprice')}</div>
           <div class="Contribute-value">40 Gwei</div>
         </div>
         <div class="Contribute-field">
-          <div class="Contribute-title">Recommended gas limit</div>
+          <div class="Contribute-title">${translated('gaslimit')}</div>
           <div class="Contribute-value">200.000</div>
         </div>
       </div>
@@ -186,16 +228,14 @@ function handleTick() {
     countdownHolder.innerHTML = `
       <div class="Countdown-content Countdown--started">
         <span class="Countdown-left">
-          The ICO has started
+          ${translated('started')}
         </span>
         <span class="Countdown-right">
-          <a class="Button" href="#contribute">Contribute now</a>
+          <a class="Button" href="#contribute">${translated('contribute')}</a>
         </span>
     `;
 
-    document.querySelector('.Section--contribute .Text').innerHTML = `
-      The contribution period is now live and it will end on the 8th of September 2017 at approximately 18:00 GMT, or when the hard cap has been reached. To contribute you must agree to our terms and click on “Contribute”.
-    `;
+    document.querySelector('.Section--contribute .Text').innerHTML = translated('live')
 
     window.requestAnimationFrame(() => {
       document
